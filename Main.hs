2004f7540ba19b656265c61363c1d11d2a4d9fa0
mrb <michaelrbernstein@gmail.com>
2014-11-03 15:38:18 -0500

    Functional!

--- Main.hs
+++ Main.hs
@@ -1,72 +1,53 @@
 -- From http://book.realworldhaskell.org/read/sockets-and-syslog.html
 module Main where
-
-import Data.Bits
+ 
 import Network.Socket
-import Network.BSD
-import Data.List
 import qualified Data.Map.Strict as M
 import Control.Monad.State
-
+ 
 type DB = M.Map String [Int]
-type HandlerFunc = SockAddr -> String -> IO ()
-
+type HandlerFunc = SockAddr -> String -> StateT DB IO DB
+ 
 ins :: String -> [Int] -> DB -> DB
-ins k v db = M.insert k v db
-
+ins = M.insert
+ 
 emptyDB :: DB
 emptyDB = M.empty
-
-updateDB :: String -> [Int] -> StateT DB IO DB
-updateDB k v = do
-  current <- get
-  put $ ins k v current
-  return current
-
-serveLog :: String              -- ^ Port number or name; 514 is default
-         -> [HandlerFunc]       -- ^ Function to handle incoming messages
+ 
+serveLog :: String       
+         -> [HandlerFunc]
          -> StateT DB IO ()
-serveLog port handlerfuncs = lift $ withSocketsDo $
-    do -- Look up the port.  Either raises an exception or returns
-       -- a nonempty list.  
-       addrinfos <- getAddrInfo 
+serveLog port handlerfuncs = do
+  db <- get
+  do addrinfos <- lift $ getAddrInfo 
                     (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
                     Nothing (Just port)
-       let serveraddr = head addrinfos
-
-       -- Create a socket
-       sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
-
-       -- Bind it to the address we're listening to
-       bindSocket sock (addrAddress serveraddr)
-
-       -- Loop forever processing incoming data.  Ctrl-C to abort.
-       procMessages sock
-    where procMessages sock =
-              do -- Receive one UDP packet, maximum length 1024 bytes,
-                 -- and save its content into msg and its source
-                 -- IP and port into addr
-                 (msg, _, addr) <- recvFrom sock 1024
-                 -- Handle it
-                 mapM (\h -> h addr msg) handlerfuncs
-                 -- And process more messages
-                 procMessages sock
-
--- A simple handler that prints incoming packets
-plainHandler :: HandlerFunc
-plainHandler addr msg = 
-    putStrLn $ "[A] From " ++ show addr ++ ": " ++ msg
-
--- Dupe handler to show multiple handlers working with mapM
-plainHandler' :: HandlerFunc
-plainHandler' addr msg =
-    putStrLn $ "[B] From " ++ show addr ++ ": " ++ msg
-
+     let serveraddr = head addrinfos
+     sock <- lift $ socket (addrFamily serveraddr) Datagram defaultProtocol
+     lift $ bindSocket sock (addrAddress serveraddr)
+     procMessages sock
+  where procMessages sock =
+            do (msg, _, addr) <- lift $ recvFrom sock 1024
+               mapM_ (\h -> h addr msg ) handlerfuncs
+               procMessages sock
+ 
+dbHandler :: HandlerFunc
+dbHandler addr msg = do
+    db <- get
+    put $ ins msg [0] db
+    get
+ 
+dbPrinter :: HandlerFunc
+dbPrinter addr msg = do
+    db <- get 
+    lift $ print (show db)
+    return db
+ 
 runServer :: String -> [HandlerFunc] -> DB -> IO ()
-runServer port handlerfuncs db = void $ runStateT (serveLog port handlerfuncs) db
-
+runServer port handlerfuncs db =
+  void $ withSocketsDo (runStateT (serveLog port handlerfuncs) db)
+ 
 main :: IO ()
 main = do
   putStrLn "[][][] ... [][][]"
-  runServer "1514" [plainHandler, plainHandler'] emptyDB
-  --serveLog "1514" [plainHandler, plainHandler']
+  runServer "1514" [dbHandler, dbPrinter] emptyDB
