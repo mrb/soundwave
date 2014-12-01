349bce5259f9af63e08dd5100a3477a9563dba34
mrb <michaelrbernstein@gmail.com>
2014-12-01 14:57:44 -0500

    Can generate arbitrary Tries

--- test/SoundwaveTest.hs
+++ test/SoundwaveTest.hs
@@ -5,11 +5,62 @@ import Soundwave
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as BC (pack, unpack)
 import qualified Data.Map.Strict as M
+import qualified Data.ByteString          as S
+import qualified Data.ByteString.Internal as S (c2w, w2c)
 import Data.Int
 import qualified Data.Trie as T
 import Test.HUnit
+import Control.Monad
+import Control.Applicative
 
 -- let name = (BL.fromStrict (BC.pack "foo"))
 -- let vm = (M.fromList [(1::Int32,100::Int32)])
 -- updateDB name vm (T.empty :: DB)
 -- updateDB name (M.fromList [(1::Int32,1000::Int32)]) (updateDB name vm (T.empty :: DB))
+
+-- from https://github.com/aslatter/qc-instances/blob/master/src/Test/QuickCheck/Instances.hs
+instance (Ord k, Arbitrary k, Arbitrary v) => Arbitrary (M.Map k v) where
+    arbitrary = M.fromList <$> arbitrary
+    shrink m = M.fromList <$> shrink (M.toList m)
+
+-- from https://github.com/aslatter/qc-instances/blob/master/src/Test/QuickCheck/Instances.hs
+instance Arbitrary BL.ByteString where
+    arbitrary = BL.pack <$> arbitrary
+    shrink xs = BL.pack <$> shrink (BL.unpack xs)
+
+-- from http://code.haskell.org/~wren/bytestring-trie/test/Data/Trie/Test.hs
+newtype Letter = Letter { unLetter :: Char }
+    deriving (Eq, Ord, Show)
+
+letters :: [Char]
+letters = ['a'..'m']
+
+instance Arbitrary Letter where
+    arbitrary = Letter `fmap` elements letters
+
+newtype Str = Str { unStr :: S.ByteString }
+    deriving (Eq, Ord)
+
+instance Show Str where
+    show (Str s) = "Str {unStr = packC2W " ++ show s ++ " }"
+
+packC2W :: String -> S.ByteString
+packC2W  = S.pack . map S.c2w
+
+instance Arbitrary Str where
+    arbitrary = sized $ \n -> do
+        k <- choose (0,n)
+        s <- vector k
+        c <- arbitrary -- We only want non-empty strings.
+        return . Str . packC2W $ map unLetter (c:s)
+
+instance (Arbitrary a) => Arbitrary (T.Trie a) where
+    arbitrary = sized $ \n -> do
+        k      <- choose (0,n)
+        labels <- map unStr `fmap` vector k
+        elems  <- vector k
+        return . T.fromList $ zip labels elems
+-- end of code from http://code.haskell.org/~wren/bytestring-trie/test/Data/Trie/Test.hs
+
+-- sample (arbitrary :: Gen (T.Trie (M.Map Int32 Int32)))
+
