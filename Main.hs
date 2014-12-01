9e7281cc610f9851a8d3f99049d7872bf9394577
mrb <michaelrbernstein@gmail.com>
2014-12-01 08:15:53 -0500

    Add cabal file, simplify handler IO

--- Main.hs
+++ Main.hs
@@ -29,7 +29,7 @@ import qualified Data.Trie as T
 type ValueMap = (M.Map Int32 Int32)
 type DB =  T.Trie ValueMap
 type Env = (Maybe Request, DB, Maybe Response)
-type HandlerFunc = (BL.ByteString, Socket, SockAddr) -> StateT Env IO Env
+type HandlerFunc = (BL.ByteString, Socket, SockAddr) -> StateT Env IO ()
 
 initServer :: String
          -> [HandlerFunc]
@@ -59,11 +59,11 @@ readFramedMessage = do
   msg <- getByteString (fromIntegral len)
   return (len, msg)
 
-parseRequestBytes :: B.ByteString -> IO Datum
+parseRequestBytes :: B.ByteString -> IO Request
 parseRequestBytes s = case messageGet (BL.fromStrict s) of
-                Right (message, x) | BL.length x == 0 ->
-                  return (request message)
-                Right (message, x) | BL.length x /= 0 ->
+                Right (request, x) | BL.length x == 0 ->
+                  return request
+                Right (request, x) | BL.length x /= 0 ->
                   error "Failed to parse request"
                 Left error_message ->
                   error $ "Failed to parse request" ++ error_message
@@ -79,25 +79,33 @@ makeResponse db =  Response {
   response = fromList (map (uncurry makeDatum) (T.toList db))
 }
 
-messageParser :: HandlerFunc
-messageParser (msg, _, _) = do
+requestParser :: HandlerFunc
+requestParser (msg, _, _) = do
   (req, db, resp) <- get
-  let (len, request) = runGet readFramedMessage msg
-  p <- lift $ parseRequestBytes request
-  let n = utf8 (name p)
-  let m = M.fromList (map (\x -> (fromIntegral (key x), fromIntegral (value x)))
-                          (toList (vector p)))
+  let (len, bytes) = runGet readFramedMessage msg
+  parsedRequest <- lift $ parseRequestBytes bytes
+  put ((Just parsedRequest), db, resp)
 
+requestRouter :: HandlerFunc
+requestRouter _ = do
+  (req, db, resp) <- get
+  let datum = (request (fromJust req))
+
+  let n = utf8 (name datum)
+  let m = M.fromList (map (\x -> (fromIntegral (key x), fromIntegral (value x)))
+                          (toList (vector datum)))
+  
   if n =~ "%" :: Bool then
-    queryData (BL.toStrict n) m (req, db, resp)
+    queryData (BL.toStrict n) (req, db, resp)
   else if n =~ "\\*" :: Bool then
-    queryData (BL.toStrict n) m (req, db, resp)
+    queryData (BL.toStrict n) (req, db, resp)
   else
     updateData n m (req, db, resp)
-  get
 
-queryData :: B.ByteString -> ValueMap -> Env -> StateT Env IO ()
-queryData n m (req, db, resp) = do
+  return ()
+
+queryData :: B.ByteString -> Env -> StateT Env IO ()
+queryData n (req, db, resp) = do
   let (b,_,_) = (n =~ "%") :: (B.ByteString, B.ByteString, B.ByteString)
   let matchedDb = (T.submap b db)
   if T.null matchedDb then
@@ -108,16 +116,20 @@ queryData n m (req, db, resp) = do
       put (req, db, (Just newResp))
 
 updateData :: BL.ByteString -> ValueMap -> Env -> StateT Env IO ()
-updateData n m (req, db, resp) = if T.member (BL.toStrict n) db then
-      do
-        let newDb = (T.insert (BL.toStrict n) (M.unionWith max m (fromJust (T.lookup (BL.toStrict n) db))) db)
-        respondAndPut (BL.toStrict n) (req, newDb, resp)
-    else
-      do
-        let newDb = T.insert (BL.toStrict n) m db
-        let respDb = T.insert (BL.toStrict n) m T.empty
-        let newResp = makeResponse respDb
-        respondAndPut (BL.toStrict n) (req, newDb, (Just newResp))
+updateData n m (req, db, resp) = do
+  let strictname = (BL.toStrict n)
+  if T.member strictname db then
+    do
+      let valMap = fromJust $ T.lookup strictname db
+      let updatedValMap = M.unionWith max m valMap
+      let newDb = T.insert strictname updatedValMap db
+      respondAndPut (BL.toStrict n) (req, newDb, resp)
+  else
+    do
+      let newDb = T.insert (BL.toStrict n) m db
+      let respDb = T.insert (BL.toStrict n) m T.empty
+      let newResp = makeResponse respDb
+      respondAndPut (BL.toStrict n) (req, newDb, (Just newResp))
 
 respondAndPut :: B.ByteString -> Env -> StateT Env IO ()
 respondAndPut key (req, newDb, resp) =
@@ -131,21 +143,22 @@ respondAndPut key (req, newDb, resp) =
       put (req, newDb, (Just newResp))
  
 printer :: HandlerFunc
-printer (msg, _, _) = do
+printer _ = do
     (req, db, resp) <- get
-    lift $ print ("[REQ] " ++ show req ++ " [DB STATE] " ++ show db ++ " [RESP] " ++ show resp)
-    return (req, db, resp)
+    lift $ print ("[REQ] " ++ show req ++ " [DB] " ++ show db ++ " [RESP] " ++ show resp)
+    put (req, db, resp)
 
 responder :: HandlerFunc
-responder (msg, sock, addr) = do
+responder (_, sock, addr) = do
   (req, db, resp) <- get
   case resp of
     Just r -> do
       len <- lift $ sendTo sock (BL.toStrict (messagePut r)) addr
-      return (req, db, resp)
+      return ()
     Nothing -> do
       len <- lift $ sendTo sock B.empty addr
-      return (req, db, resp)
+      return ()
+  put (req, db, resp)
  
 runServer :: String -> [HandlerFunc] -> Env -> IO ()
 runServer port handlerfuncs db =
@@ -154,4 +167,4 @@ runServer port handlerfuncs db =
 main :: IO ()
 main = do
   putStrLn "[][][] ... [][][]"
-  runServer "1514" [messageParser, printer, responder] (Nothing, T.empty, Nothing)
+  runServer "1514" [requestParser, requestRouter, printer, responder] (Nothing, T.empty, Nothing)
